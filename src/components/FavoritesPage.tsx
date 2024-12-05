c7c56e8a8e66f85815a2d838ca1ad46322db769d
Artem Aleshchenko <artem.aleshchenko2017@gmail.com>
2024-12-05 12:30:59 +0200

    fix bugs

--- src/components/FavoritesPage.tsx
+++ src/components/FavoritesPage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import RecipeCard from './RecipeCard';
 import { Box, Typography } from '@mui/material';
 import { observer } from 'mobx-react-lite';
